922da3806116dfdcd30e0d572c3fca7a2570d70b
soothsoothes <125677657+soothsoothes@users.noreply.github.com>
2025-07-01 11:20:09 -0400

    lint fix 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { useEffect } from "react";
 
 export default function Home() {
   const [data, setData] = useState(null);
